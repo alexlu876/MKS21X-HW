a670b3f9dcc305d10a4bb4c819430227a9961fbb
Alex Lu <alexlu876@gmail.com>
2016-12-01 23:23:26 -0500

    test

---
+++ LibraryBook.java
@@ -0,0 +1,11 @@
+abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+	
+	private String callNumber;
+	
+	public LibraryBook(String author, String title, String ISBN, String callNumber){
+		super(author, title, ISBN);
+		this.callNumber = callNumber;
+	}
+
+
+}
