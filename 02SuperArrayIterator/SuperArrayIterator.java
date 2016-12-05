537ee0716bc81a04cc5afb3d7ed49d4698f48223
Alex Lu <alexlu876@gmail.com>
2016-12-04 21:24:03 -0500

    precompilation

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,7 +1,30 @@
 import java.util.*;
 public class SuperArrayIterator implements Iterator<String>{
 	    
-	int start, end;
+	int start;
+	SuperArray a;
 	
+	public SuperArrayIterator(SuperArray a){
+		this.a = a;
+		start = 0;
+	}
+	
+	public boolean hasNext(){
+		return start < a.size();
+	}
+	
+	public String next(){
+		if(hasNext()){
+			start++;
+			return a.get(start - 1);
+		}
+		else{
+			throw new NoSuchElementExcpetion();
+		}
+	}
+	
+	public void remove(){
+		throw new UnsupportedOperationException();
+	}
 	
 }
